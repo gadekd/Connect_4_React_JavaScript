317a3b8695cbd951d4f2650464040a2a9d0b4db5
gadekd <danielwgadek@gmail.com>
2024-08-01 16:36:04 +0200

    initial commit

---
+++ src/Components/GameCircle.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const onClick = (ev, id, value) => {
+    alert('on click ' + id);
+}
+
+const GameCircle = ({id, value, children}) => {
+    console.log(id);
+    return (
+        <div onClick={(ev) => onClick(ev, id, value)}>
+            {children}
+        </div>
+    )
+}
+
+export default GameCircle;
