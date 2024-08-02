f6db6fac5153079f0439a447305002c25d4585ca
gadekd <danielwgadek@gmail.com>
2024-08-02 15:48:40 +0200

    managing states

--- src/Components/GameBoard.js
+++ src/Components/GameBoard.js
@@ -1,60 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "../Game.css";
 
 import GameCircle from "./GameCircle";
 
+const NO_PLAYER = 0;
+const PLAYER_1 = 1;
+const PLAYER_2 = 2;
+
 const GameBoard = () => {
-    return (
+    const [gameBoard, setGameBoard] = useState(Array(16).fill(NO_PLAYER));
+    const [currentPlayer, setCurrentPlayer] = useState(PLAYER_1);
+
+    const circleClicked = (id) => {
+        console.log("Circle clicked!: " + id);
+
+        const board = [...gameBoard]
+        board[id] = currentPlayer;
+        setGameBoard(board);
+
+        setCurrentPlayer(currentPlayer === PLAYER_1 ? PLAYER_2 : PLAYER_1);
+
+        console.log(gameBoard);
+        console.log(currentPlayer);
+    }
+
+    const renderCircle = id => {
+        return <GameCircle id={id} className={`player_${gameBoard[id]}`} onCircleClicked={circleClicked} />
+    }
+
+return (
         <div className="gameBoard">
-            <GameCircle id={1}>
-    
-            </GameCircle>
-            <GameCircle id={2}>
-
-            </GameCircle>
-            <GameCircle id={3}>
-
-            </GameCircle>
-            <GameCircle id={4}>
-
-            </GameCircle>
-            <GameCircle id={5}>
-
-            </GameCircle>
-            <GameCircle id={6}>
-
-            </GameCircle>
-            <GameCircle id={7}>
-
-            </GameCircle>
-            <GameCircle id={8}>
-
-            </GameCircle>
-            <GameCircle id={9}>
-    
-            </GameCircle>
-            <GameCircle id={10}>
-
-            </GameCircle>
-            <GameCircle id={11}>
-
-            </GameCircle>
-            <GameCircle id={12}>
-
-            </GameCircle>
-            <GameCircle id={13}>
-
-            </GameCircle>
-            <GameCircle id={14}>
-
-            </GameCircle>
-            <GameCircle id={15}>
-
-            </GameCircle>
-            <GameCircle id={16}>
-
-            </GameCircle>
+            {renderCircle(0)}
+            {renderCircle(1)}
+            {renderCircle(2)}
+            {renderCircle(3)}
+            {renderCircle(4)}
+            {renderCircle(5)}
+            {renderCircle(6)}
+            {renderCircle(7)}
         </div>
     )
 }
--- src/Components/GameCircle.js
+++ src/Components/GameCircle.js
@@ -2,13 +2,10 @@ import React from 'react';
 
 import '../Game.css';
 
-const onClick = (ev, id) => {
-    alert('on click ' + id);
-}
+const GameCircle = ({id, children, className, onCircleClicked}) => {
 
-const GameCircle = ({id, children}) => {
     return (
-        <div className={`gameCircle ${id % 2 === 0 ? "odd" : "even"}`} onClick={(ev) => onClick(ev, id)}>
+        <div className={`gameCircle ${className}`} onClick={() => onCircleClicked(id)}>
             {children}
         </div>
     )
